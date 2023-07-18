df4d398965afe37e75927d249fca6ce7a2b642c3
Salvador <salvador.ambriz.maya@gmail.com>
2023-07-17 22:11:16 -0600

    responsive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-const nav = document.querySelector('#nav')
-const abrir = document.querySelector('#abrir')
-const cerrar = document.querySelector('#cerrar')
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const quienes = document.querySelector("#quienes");
+const inicio = document.querySelector("#inicio");
+const propiedades = document.querySelector("#propiedades");
 
-abrir.addEventListener("click", ()=>{
+abrir.addEventListener("click", () => {
     nav.classList.add("visible");
+});
 
-})
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
 
-cerrar.addEventListener("click", ()=>{
-    nav.classList.remove("visible")
-})
+quienes.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+inicio.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+propiedades.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
